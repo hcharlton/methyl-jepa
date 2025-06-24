a04f4892432ce8702b4cceb4673e38dfc0936ebf
Chester Henry Charlton <henrycharlton1@gmail.com>
2025-06-24 11:54:56 +0200

    add train/test split functionality to make_context script

---
+++ plots/residual_cg_context_ipd_v2/plot.py
@@ -0,0 +1,61 @@
+import polars as pl
+import altair as alt
+
+
+data_path='../../data/processed/train_cg_32_200.parquet'
+
+output_path='residual_cg_context_ipd.svg'
+
+pos_means = (
+    pl.read_parquet(data_path)
+    .filter(pl.col('label')==1)
+    .select(pl.col("window_fi").alias("fwd"), pl.col("window_ri").alias("rev"))
+    .unpivot(on=["fwd", "rev"], variable_name="strand", value_name="ipd_list")
+    .with_columns(index=pl.int_ranges(start=0, end=pl.col("ipd_list").list.len()))
+    .explode("index", "ipd_list")
+    .rename({"ipd_list": "ipd"})
+    .group_by("index", "strand").agg(pl.col("ipd").mean())
+    # .with_columns(pl.lit('True').alias('methylated'))
+)
+
+neg_means = (
+    pl.read_parquet(data_path)
+    .filter(pl.col('label')==0)
+    .select(pl.col("window_fi").alias("fwd"), pl.col("window_ri").alias("rev"))
+    .unpivot(on=["fwd", "rev"], variable_name="strand", value_name="ipd_list")
+    .with_columns(index=pl.int_ranges(start=0, end=pl.col("ipd_list").list.len()))
+    .explode("index", "ipd_list")
+    .rename({"ipd_list": "ipd"})
+    .group_by("index", "strand").agg(pl.col("ipd").mean())
+    # .with_columns(pl.lit('False').alias('methylated'))
+)
+
+means = pos_means.join(
+    neg_means, on=['index', 'strand'], suffix='_neg'
+    ).with_columns((pl.col('ipd')-pl.col('ipd_neg')).alias('residual'))
+# means = pl.concat([pos_means,neg_means])
+# means.unpivot(on)
+
+chart = alt.Chart(means).mark_line().encode(
+    alt.X("index:Q", title="Position", axis=alt.Axis(tickCount=16)),
+    alt.Y("residual:Q", title="IPD Mean Difference", scale=alt.Scale(domain=(-3, 15), clamp=True)),
+    alt.Color("strand:N", title="Strand"),
+    ).properties(
+    title = "Mean IPD Residual (Meth-Unmeth) Across CG Context", 
+    width=800, 
+    height=600
+    ).configure_axis(
+    labelFontSize=12, 
+    titleFontSize=14, 
+    grid=True
+    ).configure_title(
+    fontSize=16, 
+    anchor='middle'
+    ).configure_legend(
+    titleFontSize=12, 
+    labelFontSize=11
+    )
+
+chart.save(output_path)
+# print(means.head())
+
--- src/make_contexts_df.py
+++ src/make_contexts_df.py
@@ -3,59 +3,89 @@ import polars as pl
 import re
 
 # --- Configuration ---
-BAM_PATH = "../data/raw/unmethylated_subset.bam" 
-NUM_READS_TO_PROCESS = 10000            
-WINDOW_SIZE = 64                
+POS_BAM_PATH = "../data/raw/methylated_hifi_reads.bam" 
+NEG_BAM_PATH = "../data/raw/unmethylated_hifi_reads.bam" 
+NUM_READS_TO_PROCESS = 200     
+WINDOW_SIZE = 32         
+TRAIN_PROP = 0.8     
+
 
 required_tags = {"fi", "ri", "fp", "rp"}
 
-# --- Script ---
-records = []
-flank_size = (WINDOW_SIZE - 2) // 2
+
+def bam_to_parquet(bam_path: str, num_reads: int, window_size: int, label: int):
+    records = []
+    flank_size = (window_size - 2) // 2
+
+    with pysam.AlignmentFile(bam_path, "rb", check_sq=False) as bam:
+        # Iterate through a limited number of reads for a quick test
+        for i, read in enumerate(bam):
+            if i >= num_reads:
+                break
+
+            if not all(read.has_tag(tag) for tag in required_tags):
+                continue
+            seq = read.query_sequence
+
+            # forward values
+            fi_values = read.get_tag("fi")
+            fp_values = read.get_tag("fp")
+            # reverse values
+            ri_values = read.get_tag("ri")
+            rp_values = read.get_tag("rp")
 
 
+            # Find all non-overlapping "CG" sites in the sequence
+            for match in re.finditer("CG", seq):
+                L = len(seq)
+                cg_pos = match.start()
+                win_start = cg_pos - flank_size
+                win_end = cg_pos + 2 + flank_size
+                rev_win_start = L - win_end
+                rev_win_end = L - win_start
 
-with pysam.AlignmentFile(BAM_PATH, "rb", check_sq=False) as bam:
-    # Iterate through a limited number of reads for a quick test
-    for i, read in enumerate(bam):
-        if i >= NUM_READS_TO_PROCESS:
-            break
+                # Ensure the window is fully contained within the read
+                if win_start >= 0 and win_end <= len(seq):
+                    records.append({
+                        "read_name": read.query_name,
+                        "cg_pos": cg_pos,
+                        "window_seq": seq[win_start:win_end],
+                        "window_fi": list(fi_values[win_start:win_end]),
+                        "window_fp": list(fp_values[win_start:win_end]),
+                        "window_ri": list(ri_values[rev_win_start:rev_win_end]),
+                        "window_rp": list(rp_values[rev_win_start:rev_win_end])
+                    })
 
-        if not all(read.has_tag(tag) for tag in required_tags):
-            continue
-        seq = read.query_sequence
+    df = pl.from_dicts(records).with_columns(pl.lit(label).alias('label'))
 
-        # forward values
-        fi_values = read.get_tag("fi")
-        fp_values = read.get_tag("fp")
-        # reverse values
-        ri_values = read.get_tag("ri")
-        rp_values = read.get_tag("rp")
+    return df
 
 
-        # Find all non-overlapping "CG" sites in the sequence
-        for match in re.finditer("CG", seq):
-            L = len(seq)
-            cg_pos = match.start()
-            win_start = cg_pos - flank_size
-            win_end = cg_pos + 2 + flank_size
-            rev_win_start = L - win_end
-            rev_win_end = L - win_start
+def train_test_split_lazy(
+    df: pl.DataFrame, train_prop: float = 0.8
+    ) -> tuple[pl.DataFrame, pl.DataFrame]:
+    """Split polars dataframe into two sets.
+    Args:
+        df (pl.DataFrame): Dataframe to split
+        train_fraction (float, optional): Fraction that goes to train. Defaults to 0.8
+    Returns:
+        Tuple[pl.DataFrame, pl.DataFrame]: Tuple of train and test dataframes
+    """
+    df = df.with_row_index().sample(fraction=1, seed=1337, shuffle=True)
+    df_train = df.filter(pl.col("index") < pl.col("index").max() * train_prop)
+    df_test = df.filter(pl.col("index") >= pl.col("index").max() * train_prop)
+    return df_train.drop('index'), df_test.drop('index')
 
-            # Ensure the window is fully contained within the read
-            if win_start >= 0 and win_end <= len(seq):
-                records.append({
-                    "read_name": read.query_name,
-                    "cg_pos": cg_pos,
-                    "window_seq": seq[win_start:win_end],
-                    "window_fi": list(fi_values[win_start:win_end]),
-                    "window_fp": list(fp_values[win_start:win_end]),
-                    "window_ri": list(ri_values[rev_win_start:rev_win_end]),
-                    "window_rp": list(rp_values[rev_win_start:rev_win_end])
-                })
 
-# Create a polars DataFrame from the collected records
-# The schema will be automatically inferred.
-df = pl.from_dicts(records)
+pos_df = bam_to_parquet(bam_path=POS_BAM_PATH, num_reads=NUM_READS_TO_PROCESS, window_size=WINDOW_SIZE, label=1).sample(fraction=1, seed=1337, shuffle=True)
+neg_df = bam_to_parquet(bam_path=NEG_BAM_PATH, num_reads=NUM_READS_TO_PROCESS, window_size=WINDOW_SIZE, label=0).sample(fraction=1, seed=1337, shuffle=True)
 
-df.write_parquet('../data/processed/unmethylated_cg_context_small.parquet')
+pos_train_df, pos_test_df = train_test_split_lazy(pos_df, train_prop=TRAIN_PROP)
+neg_train_df, neg_test_df = train_test_split_lazy(neg_df, train_prop=TRAIN_PROP)
+
+train_df = pl.concat([pos_train_df, neg_train_df]).sample(fraction=1, seed=1337, shuffle=True)
+test_df =  pl.concat([pos_test_df, neg_test_df]).sample(fraction=1, seed=1337, shuffle=True)
+
+
+train_df.write_parquet(f'../data/processed/train_cg_{WINDOW_SIZE}_{NUM_READS_TO_PROCESS}.parquet')
+test_df.write_parquet(f'../data/processed/test_cg_{WINDOW_SIZE}_{NUM_READS_TO_PROCESS}.parquet')
