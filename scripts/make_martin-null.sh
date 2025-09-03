8ad5c75176728ede67492d73c5d794a2a2bc9964
Chester Henry Charlton <henrycharlton1@gmail.com>
2025-09-03 15:14:53 +0200

    update inference dataset generator to include quality, make null dataset generator script for sampling data chunks regardless of cpg status, begin eda plots of inference dataset features

--- scripts/make_martin-null.sh
+++ scripts/make_martin-null.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --account mutationalscanning
-#SBATCH --time 4:00:00
-#SBATCH -c 16
+#SBATCH --time 180
+#SBATCH -c 32
 #SBATCH --mem 512g
 
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate methyl-jepa
 
-python make_inference_dataset.py -i "~/mutationalscanning/DerivedData/bam/HiFi/chimp/martin/kinetics/martin_kinetics_diploid.bam" -n 1_000_000 -c 32 -o martin_ss_v0.1
+python make_null_dataset.py -i "~/mutationalscanning/DerivedData/bam/HiFi/chimp/martin/kinetics/martin_kinetics_diploid.bam" -n 1_000_000 -p 0.0024 -c 32 -o null/martin_null_p0.024_n1m
 
 
 # BAM_PATH = "~/mutationalscanning/DerivedData/bam/HiFi/chimp/martin/kinetics/martin_kinetics_diploid.bam" 
--- scripts/make_null_dataset.py
+++ scripts/make_null_dataset.py
@@ -1,19 +1,19 @@
 import pysam
 import polars as pl
-import re
 import argparse
 import sys
 import os
+import random
 
 ### Purpose ###
-# converts a BAM file into a parquet file of CpG instances. 
+# converts a BAM file into a parquet file of null windows (windows which may or may not contain a CpG site)
 
 
 #### example usage ####
 ## short format
-# python make_inference_dataset.py -n 1000 -c 32 -o output_file_name_str -r
+# python make_null_dataset.py -n 1000 -c 32 -o output_file_name_str -r
 ## long format
-# python make_inference_dataset.py --output-name output_file_name_str --context 32 --n_reads 1000 --restrict-instances
+# python make_null_dataset.py --output-name output_file_name_str --context 32 --n_reads 1000 
 
 
 # Note for the reverse strand indexing: 
@@ -33,14 +33,13 @@ import os
 # this also has the effect of preserving the seq->kinetics causal structure
 # info here https://pacbiofileformats.readthedocs.io/en/13.1/BAM.html
 
-def bam_to_df(bam_path: str, n_reads: int, context: int, singletons: bool):
+def bam_to_df(bam_path: str, n_reads: int, p_sample: float, context: int):
     # tags in the BAM file that we need to pull out each sample
     required_tags = {"fi", "ri", "fp", "rp", "np", "sm", "sx"}
     col_data = {
         "read_name": [], 
         "seq": [], 
         "qual": [],
-        "cg_pos": [],
         "np": [],
         "sm": [],
         "sx": [],
@@ -49,26 +48,24 @@ def bam_to_df(bam_path: str, n_reads: int, context: int, singletons: bool):
         "ri": [], 
         "rp": []
         }
-    
-    # in other words, how far on each side of the CG should we gather context?
-    # for context=32, this is 15 so that the total sample is 32 units long
-    flank_size = (context - 2) // 2
 
 
     counters = {
-        "reads_processed": 0,
-        "reads_missing_tags": 0,
-        "reads_with_empty_tags": 0,
-        "cpg_sites_found": 0,
-        "cpg_filtered_out_by_window": 0,
-        "cpg_appended": 0
-        }
+            "reads_processed": 0,
+            "reads_missing_tags": 0,
+            "reads_with_empty_tags": 0,
+            "windows_processed": 0,
+            "windows_filtered_by_length": 0,
+            "windows_appended": 0
+            }
 
 
     with pysam.AlignmentFile(bam_path, "rb", check_sq=False) as bam:
         for i, read in enumerate(bam):
             if i >= n_reads and n_reads !=0:
                 break
+            if random.random() > p_sample:
+                continue
             counters["reads_processed"] += 1 # counter append
             if not all(read.has_tag(tag) for tag in required_tags):
                 counters["reads_missing_tags"] += 1 # counter append
@@ -96,55 +93,49 @@ def bam_to_df(bam_path: str, n_reads: int, context: int, singletons: bool):
                    counters["reads_with_empty_tags"] += 1 # counter append
                    continue
             # Find all non-overlapping "CG" sites in the sequence
-            for match in re.finditer("CG", seq):
-                counters["cpg_sites_found"] += 1 # counter append
-                L = len(fi_values)
-                cg_pos = match.start()
-                win_start = cg_pos - flank_size
-                win_end = cg_pos + 2 + flank_size
+            L = len(fi_values)
+            for win_start in range(0, L, context):
+                counters["windows_processed"] += 1 # counter append
+                win_end = win_start + context
+
                 # perform reverse strand indexing calculation
                 rev_win_start = L - win_end
                 rev_win_end = L - win_start
+                # forward slicing 
                 context_seq = seq[win_start:win_end]
                 context_qual = qual_values[win_start:win_end]
-                # logic for only including one CG per sample (singleton)
-                if (singletons == True) and (context_seq.count("CG") != 1):
-                    continue
                 context_fi = fi_values[win_start:win_end]
                 context_fp = fp_values[win_start:win_end]
-                context_ri = ri_values[rev_win_start:rev_win_end]
-                context_rp = rp_values[rev_win_start:rev_win_end]
-                # these do not have funny indexing since they are not strand specific
                 context_sm = sm_values[win_start:win_end]
                 context_sx = sx_values[win_start:win_end]
 
+                # reverse slicing
+                context_ri = ri_values[rev_win_start:rev_win_end]
+                context_rp = rp_values[rev_win_start:rev_win_end]
+
                 # make sure they all have the same length
                 if set(map(len, [context_seq, context_qual, context_fi, context_fp, context_ri, context_rp, context_sm, context_sx])) != {context}:
-                    counters["cpg_filtered_out_by_window"] += 1 # counter append
+                    counters["windows_filtered_by_length"] += 1 # counter append
                     continue
-                # Ensure the window is fully contained within the read
-                if all([win_start >= 0, win_end <= L, rev_win_end >=0, rev_win_start <= L]):
-                    counters["cpg_appended"] += 1 # counter append
-                    col_data["read_name"].append(read.query_name)
-                    col_data["cg_pos"].append(cg_pos)
-                    col_data["seq"].append(context_seq)
-                    col_data['qual'].append(context_qual)
-                    col_data["fi"].append(context_fi)
-                    col_data["fp"].append(context_fp)
-                    col_data["ri"].append(context_ri)
-                    col_data["rp"].append(context_rp)
-                    col_data["np"].append(np_value)
-                    col_data["sm"].append(context_sm)
-                    col_data["sx"].append(context_sx)
+                counters["windows_appended"] += 1 # counter append
+                col_data["read_name"].append(read.query_name)
+                col_data["seq"].append(context_seq)
+                col_data['qual'].append(context_qual)
+                col_data["fi"].append(context_fi)
+                col_data["fp"].append(context_fp)
+                col_data["ri"].append(context_ri)
+                col_data["rp"].append(context_rp)
+                col_data["np"].append(np_value)
+                col_data["sm"].append(context_sm)
+                col_data["sx"].append(context_sx)
     # use List(UInt16) for memory saving since polars defaults to UInt64
 
     print("--- Debugging Counters ---")
     for key, value in counters.items():
-        print(f"{key:<25}: {value}")
+        print(f"{key:<30}: {value}")
     print("--------------------------")
     df = pl.DataFrame({
         "read_name": pl.Series(col_data["read_name"], dtype = pl.String),
-        "cg_pos": pl.Series(col_data["cg_pos"], dtype = pl.UInt16),
         "seq": pl.Series(col_data["seq"], dtype = pl.String),
         "qual": pl.Series(col_data['qual'], dtype = pl.List(pl.UInt8)),
         "np": pl.Series(col_data["np"], dtype = pl.UInt8),
@@ -173,6 +164,11 @@ def main():
                         type=int,
                         required=True,
                         help="Number of reads to process. 0 for all reads.")
+    parser.add_argument('-p', '--p_sample',
+                        type=float,
+                        required=True,
+                        help="Probability to collect data from a read. Set to 1 for all reads."
+                        )
     parser.add_argument('-c', '--context',
                         type=int,
                         required=True,
@@ -181,9 +177,6 @@ def main():
                         type=str,
                         required=True,
                         help="The output file name")
-    parser.add_argument('--singletons',
-                        action='store_true',
-                        help="If specified, restrict samples to contain only one CG instance. Defaults to False if not specified.")
     args = parser.parse_args()
     if args.n_reads < 0:
         print("Error: n_reads should be positive or 0 (to indicate all reads).")
@@ -191,8 +184,9 @@ def main():
 
     df = bam_to_df(bam_path=os.path.expanduser(args.input_path), 
                             n_reads=args.n_reads, 
-                            context=args.context, 
-                            singletons=args.singletons)
+                            p_sample=args.p_sample,
+                            context=args.context,
+                            )
 
 
 
